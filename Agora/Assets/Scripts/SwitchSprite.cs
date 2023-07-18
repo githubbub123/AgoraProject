4f13327fccf7143d76ed3011c0e7cc2daa54005d
charlieirosee <139798697+charlieirosee@users.noreply.github.com>
2023-07-18 11:55:25 -0400

    turn around yay

--- Agora/Assets/Scripts/SwitchSprite.cs
+++ Agora/Assets/Scripts/SwitchSprite.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SwitchSprite : MonoBehaviour
 {
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRend;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRend = GetComponent<SpriteRenderer>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W)) {
+            spriteRend.sprite = sprites[0];
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            spriteRend.sprite = sprites[1];
+        }
     }
 }
