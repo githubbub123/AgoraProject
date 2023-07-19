52b9dd936245b9e2bbd644ebd51d9a974c9e9f33
githubbub123 <githubbub123@gmail.com>
2023-07-19 10:33:43 -0400

    FInished player interaction

--- Agora/Assets/Scripts/CanInteractWith.cs
+++ Agora/Assets/Scripts/CanInteractWith.cs
@@ -6,5 +6,7 @@ public class CanInteractWith : MonoBehaviour
 {
     // Interaction message
     public string[] interactionMessage;
-    public bool canPush;
+    public string[] choices;
+    public int interactionId;
+
 }
