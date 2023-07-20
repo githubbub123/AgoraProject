3e2b7becb943f6608559b4257f95aaeb9dd4c911
charlieirosee <139798697+charlieirosee@users.noreply.github.com>
2023-07-20 10:39:42 -0400

    waietrydf

--- Agora/Assets/Scripts/ShowText.cs
+++ Agora/Assets/Scripts/ShowText.cs
@@ -7,12 +7,19 @@ public class ShowText : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MakeTextAppear());
     }
+
+    private IEnumerator MakeTextAppear() {
+        yield return new WaitForSeconds(1f);
+        string[] text = { "bavaf}" };
+        StartCoroutine(GameManager.gm.AppearBackground(text));
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //FindObjectOfType<GameManager>().AppearBackground(choiceBox));
     }
 }
